bc249a4f56e93b4c5555af8c14915c12a3a0cbcc
CibNumeritos <85264290+CibNumeritos@users.noreply.github.com>
2022-04-19 20:13:08 -0500

    Update index.js
    Changed command function

--- scripts/index.js
+++ scripts/index.js
@@ -3,33 +3,50 @@ This file belongs to: "@CibNumeritos" (Eternetic Studios), any unauthorized modi
 For more information to authorize your copies, you can contact him on discord: https://discord.gg/b39ncqJQWU 
 or contact him via Twitter: https://twitter.com/CibNumeritos - Don't share direct download links.
 */
-import { world } from 'mojang-minecraft';
+import { world, BeforeChatEvent } from 'mojang-minecraft';
 let prefix = "!";
 world.events.beforeChat.subscribe(data => {
     if (data.message.startsWith(prefix)) { 
         commands(data);
     };
 });
-world.events.tick.subscribe(sleepHandler);
-function sleepHandler() {
-};
+world.events.tick.subscribe(tick => {});
+/**
+ * 
+ * @param {BeforeChatEvent} data 
+ * @returns 
+ */
 function commands(data) {
-    let command = data.message.match(/(?<=!)\w+/)[0];
-    const params = data.message.match(/(?<= )[\w&_-]*/g);
+    let command = data.message.match(new RegExp('(?<=' + `\\${prefix}` + ')\\w+'))?.[0].toLowerCase();
+    let origin = data.sender;
+    let output = `The command was executed. (${origin.nameTag})`;
+    let params = data.message.split(' '); params.splice(0, 1);
+    let quotes = data.message.match(/(?<=\").*?(?=\")/g);
+    selector = Array.from(world.getPlayers()).find(sel => {
+        sel.nameTag 
+        == 
+        data.message
+                .match(/(?<=\@).?/)
+                ?.[0]
+                .replace(/\"/g, '');
+    });
     switch (command) {
         case "sleep": {
-            if (!data.sender.hasTag('op') && params[0] == "setplayers") {
-                world.getDimension('overworld').runCommand(`tellraw "${data.sender.nameTag}" {"rawtext":[{"text":"§c"},{"translate":"commands.generic.unknown", "with": ["${command}"]}]}`);
-                return;
-            } else if (params[0] == "setplayers") {
-                world.getDimension('overworld').runCommand(`scoreboard players set uopMinPlayers uopmp ${params[1]}`);
-            } else {
-                world.getDimension('overworld').runCommand(`tellraw @a {"rawtext":[{"translate":"commands.generic.syntax","with":["${params[0]}","${command}"]}]}`);
+            switch (true) {
+                case !data.sender.hasTag('op') && params[0] == "setplayers": {
+                    world.getDimension('overworld').runCommand(`tellraw "${data.sender.nameTag}" {"rawtext":[{"text":"§c"},{"translate":"commands.generic.unknown", "with": ["${command}"]}]}`);
+                } break;
+                case params[0] == "setplayers": {
+                    world.getDimension('overworld').runCommand(`scoreboard players set uopMinPlayers uopmp ${params[1]}`);
+                } break;
+                default: {
+                    world.getDimension('overworld').runCommand(`tellraw @a {"rawtext":[{"translate":"commands.generic.syntax","with":["${params[0]}","${command}"]}]}`);
+                } break;
             };
         } break;
         case "sleeprequest": {
             world.getDimension('overworld').runCommand(`tellraw @a {"rawtext":[{"translate":"cib.sleep.request","with":["${data.sender.nameTag}"]}]}`);
-        };
+        } break;
         default: {
             world.getDimension('overworld').runCommand(`tellraw "${data.sender.nameTag}" {"rawtext":[{"text":"§c"},{"translate":"commands.generic.unknown", "with": ["${command}"]}]}`);
         };
