ae39cabfd824f8f32e2d8ffe0ddbfc252db7f90d
CibNumeritos <85264290+CibNumeritos@users.noreply.github.com>
2022-04-20 21:50:40 -0500

    Added translations, index.js updated and resourcepack added

--- bp/scripts/index.js
+++ bp/scripts/index.js
@@ -4,6 +4,17 @@ For more information to authorize your copies, you can contact him on discord: h
 or contact him via Twitter: https://twitter.com/CibNumeritos - Don't share direct download links.
 */
 import { world, BeforeChatEvent } from 'mojang-minecraft';
+world.getDimension('overworld').runCommand(`scoreboard players add uopsdb`);
+try {
+    world.getDimension('overworld').runCommand(`scoreboard players set uopsMinPlayers uopsdb 1`);
+    world.getDimension('overworld').runCommand(`scoreboard players set uopsSleepingPlrs uopsdb 0`);
+    world.getDimension('overworld').runCommand(`scoreboard players set uopsTimeAdd uopsdb 50`);
+} catch (error) {
+    console.warn(error);
+};
+let minPlayers;
+let sleepingPlayers;
+let timeAdd;
 let prefix = "!";
 world.events.beforeChat.subscribe(data => {
     if (data.message.startsWith(prefix)) { 
@@ -11,7 +22,34 @@ world.events.beforeChat.subscribe(data => {
         data.cancel = true;
     };
 });
-world.events.tick.subscribe(tick => {});
+world.events.tick.subscribe((currentTick) => {
+    minPlayers = parseInt(world.getDimension('overworld').runCommand(`scoreboard players test uopsMinPlayers uopsdb * *`));
+    sleepingPlayers = parseInt(world.getDimension('overworld').runCommand(`scoreboard players test uopsSleepingPlrs uopsdb * *`));
+    timeAdd = parseInt(world.getDimension('overworld').runCommand(`scoreboard players test uopsTimeAdd uopsdb * *`));
+    if (isNight() && sleepingPlayers >= minPlayers) {
+        if (currentTick % 20 === 10) {
+            world.getDimension('overworld').runCommand(`time add ${timeAdd}`);
+        };
+    };
+});
+let getTime = () => {
+    try {
+        let timeQuery = parseInt(world.getDimension('overworld').runCommand(`time query daytime`));
+        return timeQuery;
+    } catch (error) {
+        return { error: false, ...error};
+    };
+};
+let isNight = () => {
+    try {
+        let timeQuery = parseInt(world.getDimension('overworld').runCommand(`time query daytime`));
+        if (timeQuery >= 12542) {
+            return true;
+        };
+    } catch (error) {
+        return { error: false, ...error };
+    };
+};
 /**
  * 
  * @param {BeforeChatEvent} data 
@@ -30,22 +68,40 @@ function commands(data) {
                 ?.[0]
                 .replace(/\"/g, '');
     });
-    switch (command) {
-        case `${prefix}sleep`: {
+    switch (true) {
+        case command == `${prefix}sleep` || command == `${prefix}s`: {
             switch (true) {
                 case !data.sender.hasTag('op') && params[0] == "setplayers": {
-                    origin.runCommand(`tellraw "${data.sender.nameTag}" {"rawtext":[{"text":"§c"},{"translate":"commands.generic.unknown", "with": ["${command}"]}]}`);
+                    origin.runCommand(`tellraw @s {"rawtext":[{"translate":"commands.generic.syntax","with":["${params[0]}","${command}"]}]}`);
                 } break;
                 case params[0] == "setplayers": {
-                    origin.runCommand(`scoreboard players set uopMinPlayers uopmp ${params[1]}`);
+                    if (!params[0]) {
+                        return origin.runCommand(`tellraw @s {"rawtext":[{"translate":"cib.customcommand.missingparam.sleepsetplayers","with":[""]}]}`);
+                    };
+                    origin.runCommand(`scoreboard players set uopsMinPlayers uopsdb ${params[1]}`);
                     origin.runCommand(`tellraw @s {"rawtext":[{"translate":"cib.customcommand.succes.sleepsetplayers","with":["${params[0]}"]}]}`);
+                } break;
+                case params[0] == "timeadd": {
+                    let check = parseInt(params?.[0]);
+                    switch (true) {
+                        case !params[0]: {
+                            origin.runCommand(`tellraw @s {"rawtext":[{"translate":"cib.customcommand.missingparam.sleeptimeadd","with":[""]}]}`);
+                        } break;
+                        case check == NaN: {
+                            origin.runCommand(`tellraw @s {"rawtext":[{"translate":"cib.customcommand.notanumber.sleeptimeadd","with":["${params[0]}"]}]}`);
+                        } break;
+                        default: {
+                            origin.runCommand(`scoreboard players set uopsTimeAdd uopsdb ${check}`);
+                            origin.runCommand(`tellraw @s {"rawtext":[{"translate":"cib.customcommand.succes.sleeptimeadd","with":["${check}"]}]}`);
+                        } break;
+                    };
                 } break;
                 default: {
                     origin.runCommand(`tellraw @s {"rawtext":[{"translate":"commands.generic.syntax","with":["${params[0]}","${command}"]}]}`);
                 } break;
             };
         } break;
-        case `${prefix}sleeprequest`: {
+        case command == `${prefix}sleeprequest` || command == `${prefix}sr`: {
             origin.runCommand(`tellraw @s {"rawtext":[{"translate":"cib.sleep.request","with":["${data.sender.nameTag}"]}]}`);
         } break;
         default: {
